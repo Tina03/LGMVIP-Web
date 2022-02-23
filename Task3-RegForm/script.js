2918ee31ee7fa975134d042c474c6f1065855b16
Tina03 <91134543+Tina03@users.noreply.github.com>
2022-02-23 17:08:24 +0530

    Add files via upload

--- Task3-RegForm/script.js
+++ Task3-RegForm/script.js
@@ -14,26 +14,33 @@ function onFormSubmit(e) {
 function readFormData() {
   var formData = {};
   formData["rollNo"] = document.getElementById("rollNo").value;
-  formData["studentName"] = document.getElementById("studentName").value;
-  formData["Mob"] = document.getElementById("Mob").value;
+  formData["Name"] = document.getElementById("Name").value;
+  formData["Email"] = document.getElementById("Email").value;
   formData["Branch"] = document.getElementById("Branch").value;
   formData["Course"] = document.getElementById("Course").value;
   return formData;
 }
 
 function insertNewRecord(data) {
-  var table = document.getElementById("storeList").getElementsByTagName("tbody")[0];
+  var table = document
+    .getElementById("storeList")
+    .getElementsByTagName("tbody")[0];
   var newRow = table.insertRow(table.length);
   cell1 = newRow.insertCell(0);
   cell1.innerHTML = data.rollNo;
+
   cell2 = newRow.insertCell(1);
-  cell2.innerHTML = data.studentName;
+  cell2.innerHTML = data.Name;
+
   cell3 = newRow.insertCell(2);
-  cell3.innerHTML = data.Mob;
+  cell3.innerHTML = data.Email;
+
   cell4 = newRow.insertCell(3);
   cell4.innerHTML = data.Branch;
+
   cell5 = newRow.insertCell(4);
   cell5.innerHTML = data.Course;
+
   cell6 = newRow.insertCell(5);
   cell6.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
 }
@@ -41,18 +48,17 @@ function insertNewRecord(data) {
 function onEdit(td) {
   selectedRow = td.parentElement.parentElement;
   document.getElementById("rollNo").value = selectedRow.cells[0].innerHTML;
-  document.getElementById("studentName").value = selectedRow.cells[1].innerHTML;
-  document.getElementById("Mob").value = selectedRow.cells[2].innerHTML;
+  document.getElementById("Name").value = selectedRow.cells[1].innerHTML;
+  document.getElementById("Email").value = selectedRow.cells[2].innerHTML;
   document.getElementById("Branch").value = selectedRow.cells[3].innerHTML;
   document.getElementById("Course").value = selectedRow.cells[4].innerHTML;
-
 }
 function updateRecord(formData) {
-  selectedRow.cells[0].innerHTML = formData.rollNo;
-  selectedRow.cells[1].innerHTML = formData.studentName;
-  selectedRow.cells[2].innerHTML = formData.Mob;
-  selectedRow.cells[3].innerHTML = formData.Branch;
-  selectedRow.cells[4].innerHTML = formData.Course;
+  selectedRow.cells[0].innerHTML = formData.studentName;
+  selectedRow.cells[1].innerHTML = formData.collegeName;
+  selectedRow.cells[2].innerHTML = formData.Branch;
+  selectedRow.cells[3].innerHTML = formData.Mob;
+  selectedRow.cells[4].innerHTML = formData.Addr;
 }
 
 function onDelete(td) {
@@ -65,8 +71,8 @@ function onDelete(td) {
 
 function resetForm() {
   document.getElementById("rollNo").value = "";
-  document.getElementById("studentName").value = "";
-  document.getElementById("Mob").value = "";
+  document.getElementById("Name").value = "";
+  document.getElementById("Email").value = "";
   document.getElementById("Branch").value = "";
   document.getElementById("Course").value = "";
 
